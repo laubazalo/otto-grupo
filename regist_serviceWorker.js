a73881074eb9bc9b13d5e18660d30f6e71b62774
mbiancadepascual <mbiancadepascual@gmail.com>
2022-08-27 16:57:00 -0300

    Nuevos archivos

--- regist_serviceWorker.js
+++ regist_serviceWorker.js
@@ -1,7 +1,7 @@
 /*Registra un servicio*/
 /*Hace la invocación al servicio Service_Worker*/
-if('ServiceWorker' in navigator){
+if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('./Service_Worker.js')
-    .then(reg => console.log('Registro exitoso', reg))
-    .catch(err => console.warn('Error al tratar de registrar al sw', err))
+      .then(reg => console.log('Registro de SW exitoso', reg))
+      .catch(err => console.warn('Error al tratar de registrar el sw', err))
 }
