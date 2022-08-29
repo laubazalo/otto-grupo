81bafc1935a7b2bb6b9fc21a15ee9c87a5b66a60
agoscorrea <agos_13correa@hotmail.com>
2022-08-29 18:26:49 -0300

    iniciando pruebas de servos

--- prueba/prueba.ino
+++ prueba/prueba.ino
@@ -1,12 +1,21 @@
 #include <SoftwareSerial.h>
+#include <Servo.h>
 
 //d0: RX
 //d1: TX
+//pie1: pin2
 
+Servo pie1; 
 SoftwareSerial miBT(10, 11);
+
+int PINPIE1 = 2; 
+int PULSOMIN = 1000;
+int PULSOMAX = 2000;
+int comando = 0;
 
 void setup() {
   // put your setup code here, to run once:
+  pie1.attach(PINPIE1);
   Serial.begin(9600);
   miBT.begin(9600);
 }
@@ -15,9 +24,11 @@ void loop() {
   // put your main code here, to run repeatedly:
 
   if (miBT.available()) {   // if HM10 sends something then read
-
-    Serial.println(miBT.readString());
-
+    comando = miBT.parseInt();
+    Serial.println(comando);
   }
   
+  pie1.write(comando);
+  delay(1000);
+  comando = 0;
 }
