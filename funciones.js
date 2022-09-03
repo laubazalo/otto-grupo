cd3a70629d2be3ccb693195fee7f0f0ba5ea4d88
mbiancadepascual <mbiancadepascual@gmail.com>
2022-09-03 16:07:31 -0300

    Nuevos archivos 3-9

--- funciones.js
+++ funciones.js
@@ -1,16 +1,13 @@
 function run(){
   let options = {};
-  //if (document.querySelector('#allDevices').checked) {
-    options.acceptAllDevices = true;
- // } else {
-  //  options.filters = filters;
-//  }
+  options.acceptAllDevices = true;
+
   console.log('Llega');
   navigator.bluetooth.requestDevice(options) //Llama a navigator.bluetooth.requestDevice
   .then(device => {
     console.log('Nombre' + device.name);
     return device.gatt.connect(); //Intenta conectarse al servidor GATT remoto
   })
-  //.then(server => { /* … */ })
   .catch(error => { console.error(error); });
+  
 }
